6109021c67aa7c034f08d9dd9d7d3a5c4a205635
Lindsay Thomas <lindsay_thomas@esri.com>
2015-06-03 13:16:58 -0400

    new WMA reference

--- js/configWMALocator.js
+++ js/configWMALocator.js
@@ -67,7 +67,7 @@ define([], function () {
         ApplicationName: "Wildlife Management Area Locator",
 
         // Set application icon path
-        ApplicationIcon: "/js/library/themes/images/logo.png",
+        ApplicationIcon: "/js/library/themes/images/WMAlogo.png",
 
         // Set application Favicon path
         ApplicationFavicon: "/js/library/themes/images/favicon.ico",
