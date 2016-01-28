8bb14e1d2d11accce0529cc8b9c37f1db622c959
Lindsay King <lking@esri.com>
2016-01-28 12:02:52 -0800

    Update configParkLocator.js
    Updated to remove portal from comments #226

--- js/configParkLocator.js
+++ js/configParkLocator.js
@@ -38,7 +38,7 @@ define([], function () {
         // 14. Bottom Panel InfoPod Settings                 - [ Tag(s) to look for: PodSettings ]
         // 15. Customize Zoom level for address search       - [ Tag(s) to look for: ZoomLevel ]
         // 16. Specify WebMap Id                             - [ Tag(s) to look for: WebMapId ]
-        // 17. Specify URL to ArcGIS Portal REST API         - [ Tag(s) to look for: PortalAPIURL ]
+        // 17. Specify URL to ArcGIS Online REST API         - [ Tag(s) to look for: PortalAPIURL ]
         // 18. Specify the Group Title that contains basemaps- [ Tag(s) to look for: BasemapGroupTitle ]
         // 19. Specify the Group Name that contains basemaps - [ Tag(s) to look for: BasemapGroupOwner ]
         // 20. Specify Spatial Reference for basemaps        - [ Tag(s) to look for: BasemapSpatialReferenceWKID ]
@@ -141,7 +141,7 @@ define([], function () {
         // Set options for basemap
         // Please note: All basemaps need to use the same spatial reference.
 
-        // Specify URL to ArcGIS Portal REST API. If you are using ArcGIS Online, leave this parameter as is.
+        // Specify URL to ArcGIS Online REST API
         PortalAPIURL: "http://www.arcgis.com/sharing/rest/",
 
         // Specify the Title of Group that contains basemaps
@@ -153,7 +153,7 @@ define([], function () {
         // Specify Spatial Reference for basemaps, since all basemaps need to use the same spatial reference
         BasemapSpatialReferenceWKID: 102100,
 
-        // Specify path of the image used to display the Thumbnail for a basemap when portal does not provide it
+        // Specify path of the image used to display the Thumbnail for a basemap when ArcGIS Online does not provide it
         NoThumbnail: "js/library/themes/images/not-available.png",
 
         // ------------------------------------------------------------------------------------------------------------------------
